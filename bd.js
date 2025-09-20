538ecc220d7dfac29291f7cf1fc70e85db27939a
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2025-09-20 01:25:00 +0000

    Exemplo

--- bd.js
+++ bd.js
@@ -2,3 +2,36 @@ const usuario = {
     "email": "admin@email.com",
     "senha": "admin"
 };
+
+const dados = [
+    {
+        "id": ,
+        "titulo": ,
+        "descricao": ,
+        "conteudo": ,
+        "categoria": ,
+        "autor": ,
+        "data": ,
+        "imagem": "./asset/imagem/"
+    },
+    {
+        "id": ,
+        "titulo": ,
+        "descricao": ,
+        "conteudo": ,
+        "categoria": ,
+        "autor": ,
+        "data": ,
+        "imagem": "./asset/imagem/"
+    },
+    {
+        "id": ,
+        "titulo": ,
+        "descricao": ,
+        "conteudo": ,
+        "categoria": ,
+        "autor": ,
+        "data": ,
+        "imagem": "./asset/imagem/"
+    }
+];
