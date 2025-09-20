474944164b834675fcbd23e17068e954bf904a5d
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2025-09-20 01:37:46 +0000

    ExemploA

--- bd.js
+++ bd.js
@@ -9,9 +9,9 @@ const dados = [
         "titulo": "Mobilidade Urbana em Belo Horizonte",
         "descricao": "Estudos para definição de políticas e de uma carteira de projetos e ações para melhoria das condições de mobilidade urbana.",
         "conteudo": "O Governo do Estado de Minas Gerais iniciou a elaboração do Plano de Mobilidade da Região Metropolitana de Belo Horizonte em outubro de 2018. O plano tem como objetivo apresentar estudos para definição de políticas e de uma carteira de projetos e ações para melhoria das condições de mobilidade urbana na RMBH e sua sustentabilidade. Ele terá como resultado a construção de planos de caráter multimodal para o Transporte Coletivo Metropolitano, Logística Urbana Metropolitana, Transporte individual motorizado e Transporte ativo. Serão considerados como temas transversais o uso e ocupação do solo, sustentabilidade, segurança viária, acessibilidade, governança e participação popular.",
-        "categoria": ,
-        "autor": ,
-        "data": ,
+        "categoria": "Transporte",
+        "autor": "Ana Maria Silva",
+        "data": "19/09/2025",
         "imagem": "./asset/imagem/mobilidade.png"
     },
     {
