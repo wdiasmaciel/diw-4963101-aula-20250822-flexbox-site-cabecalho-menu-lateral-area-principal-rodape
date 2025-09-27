a8af5d4b303a974a0928c58f9ff02c27720e64e6
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2025-09-27 00:10:22 +0000

    Exemplo

--- bd.js
+++ bd.js
@@ -5,9 +5,9 @@ const usuario = {
 
 const dados = [
     {
-        "id": 1,
+        "id": 0,
         "titulo": "Mobilidade Urbana em Belo Horizonte",
-        "descricao": "Estudos para definição de políticas e de uma carteira de projetos e ações para melhoria das condições de mobilidade urbana.",
+        "descricao": "Estudos para definição de políticas para melhoria da mobilidade urbana.",
         "conteudo": "O Governo do Estado de Minas Gerais iniciou a elaboração do Plano de Mobilidade da Região Metropolitana de Belo Horizonte em outubro de 2018. O plano tem como objetivo apresentar estudos para definição de políticas e de uma carteira de projetos e ações para melhoria das condições de mobilidade urbana na RMBH e sua sustentabilidade. Ele terá como resultado a construção de planos de caráter multimodal para o Transporte Coletivo Metropolitano, Logística Urbana Metropolitana, Transporte individual motorizado e Transporte ativo. Serão considerados como temas transversais o uso e ocupação do solo, sustentabilidade, segurança viária, acessibilidade, governança e participação popular.",
         "categoria": "Transporte",
         "autor": "Ana Maria Silva",
@@ -15,23 +15,23 @@ const dados = [
         "imagem": "./asset/imagem/mobilidade.png"
     },
     {
-        "id": 2,
+        "id": 1,
         "titulo": "Sexta Geração de Tecnologias de Comunicação sem Fio",
-        "descricao": ,
-        "conteudo": ,
-        "categoria": ,
-        "autor": ,
-        "data": ,
+        "descricao": "A tecnologia 6G é a evolução da rede 5G.",
+        "conteudo": "A tecnologia 6G é a evolução da rede 5G, prevendo-se a sua chegada comercial por volta de 2030, com velocidades até 1 Tbps, latência em microssegundos e a capacidade de conectar bilhões de dispositivos simultaneamente. Embora ainda em fase de pesquisa, o 6G promete revolucionar a conectividade ao integrar inteligência artificial e sensores, possibilitando aplicações como comunicação holográfica, gêmeos digitais e cidades inteligentes.",
+        "categoria": "Tecnologia",
+        "autor": "Bruno Costa e Silva",
+        "data": "26/09/2025",
         "imagem": "./asset/imagem/tecnologia-6g.png"
     },
     {
-        "id": 3,
+        "id": 2,
         "titulo": "Festival de Música de Belo Horizonte",
-        "descricao": ,
-        "conteudo": ,
-        "categoria": ,
-        "autor": ,
-        "data": ,
+        "descricao": "Diversas opções para diferentes gostos musicais.",
+        "conteudo": "Existem vários festivais de música em Belo Horizonte, que abrangem diversos gêneros musicais.",
+        "categoria": "Cultura",
+        "autor": "Carla Vieira Santos",
+        "data": "24/09/2025",
         "imagem": "./asset/imagem/festival-de-musica.png"
     }
 ];
