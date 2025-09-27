97e3d01660c862b2ddbdb6c479c3e2be0159fcee
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2025-09-27 00:58:15 +0000

    Exemplo

--- noticia.js
+++ noticia.js
@@ -22,8 +22,16 @@ function carregarNoticias() {
 
         cartao.innerHTML = `
           <img src="${item.imagem}" alt="${item.titulo}">
-          
+          <section class="corpo-cartao">
+            <h2 class="titulo-noticia-cartao">${item.titulo}</h2>
+            <p>${item.categoria} - ${item.data}</p>
+            <p class="descricao-noticia-cartao">${item.descricao}</p>
+            <p class="autor-noticia-cartao">${item.autor}</p>
+          </section>
+          <a class="botao-cartao" href="detalhes.html?id=${item.id}">Leia mais</a>
         `;
+
+        container.appendChild(cartao);
     });
 }
 
