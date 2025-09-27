ce3b9b01736909ac98ad226332dcd10294b32da3
Wesley Dias Maciel <wdiasmaciel@gmail.com>
2025-09-27 00:48:50 +0000

    Exemplo

--- noticia.js
+++ noticia.js
@@ -1,11 +1,34 @@
 let noticiasCarregadas = false;
 
 function lerNoticias() {
-    
-    if(!noticiasCarregadas) {
-       carregarNoticias();
-       noticiasCarregadas = true;
-    } else {
+    const container = document.getElementById("container");
 
+    if (!noticiasCarregadas) {
+        carregarNoticias();
+        noticiasCarregadas = true;
+    } else {
+        container.innerHTML = "";
+        noticiasCarregadas = false;
     }
 }
+
+function carregarNoticias() {
+    const container = document.getElementById("container");
+
+    dados.forEach(item => {
+        const cartao = document.createElement("div");
+
+        cartao.className = "cartao";
+
+        cartao.innerHTML = `
+          <img src="${item.imagem}" alt="${item.titulo}">
+          
+        `;
+    });
+}
+
+
+
+
+
+
